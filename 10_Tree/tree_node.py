9dfe84a5de51361f405343b3fa79e6c04a53bbfb
knromaric <nzekeng@gmail.com>
2025-11-17 16:17:15 +0100

    Added the class representing a tree Node

---
+++ 10_Tree/tree_node.py
@@ -0,0 +1,22 @@
+from logging.config import valid_ident
+
+
+class Node: 
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+a = Node ('a')
+b = Node ('b')
+c = Node ('c')
+d = Node ('d')
+e = Node ('e')
+f = Node ('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f 
